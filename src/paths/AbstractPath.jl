77498d673bbb1e6179f748d82c5de93a530bf380
Sebastian Schlenkrich <sebastian.schlenkrich@frame-consult.de>
2023-08-08 14:39:27 +0200

    Add Monte-Carlo Path and methods

---
+++ src/paths/AbstractPath.jl
@@ -0,0 +1,240 @@
+
+"""
+    abstract type AbstractPath end
+
+An AbstractPath specifies the interface for path implementations.
+
+This aims at providing the flexibility to add other types of paths in
+the future.
+"""
+abstract type AbstractPath end
+
+"""
+    length(p::AbstractPath)
+
+Return the number of realisations represented by the AbstractPath
+object.
+
+We assume that model functions applied to an AbstractPath return a
+vector of length(p) where p is the number realisations.
+"""
+function length(p::AbstractPath)
+    error("AbstractPath needs to implement length method.")
+end
+
+"""
+    numeraire(p::AbstractPath, t::ModelTime, curve_key::String)
+
+Calculate the numeraire in the domestic currency.
+
+We allow for curve-specific numeraire calculation e.g. to allow
+for trade-specific discounting in AMC valuation.
+"""
+function numeraire(p::AbstractPath, t::ModelTime, curve_key::String)
+    error("AbstractPath needs to implement numeraire method.")
+end
+
+
+"""
+    bank_account(p::AbstractPath, t::ModelTime, key::String)
+
+Calculate a continuous compounded bank account value.
+"""
+function bank_account(p::AbstractPath, t::ModelTime, key::String)
+    error("AbstractPath needs to implement bank_account method.")
+end
+
+
+"""
+    zero_bond(p::AbstractPath, t::ModelTime, T::ModelTime, key::String)
+
+Calculate a zero coupon bond price.
+"""
+function zero_bond(p::AbstractPath, t::ModelTime, T::ModelTime, key::String)
+    error("AbstractPath needs to implement zero_bond method.")
+end
+
+
+"""
+    asset(p::AbstractPath, t::ModelTime, key::String)
+
+Calculate asset price.
+"""
+function asset(p::AbstractPath, t::ModelTime, key::String)
+    error("AbstractPath needs to implement asset method.")
+end
+
+
+"""
+    forward_asset(p::AbstractPath, t::ModelTime, T::ModelTime, key::String)
+
+Calculate forward asset price as expectation in T-forward measure.
+"""
+function forward_asset(p::AbstractPath, t::ModelTime, T::ModelTime, key::String)
+    error("AbstractPath needs to implement forward_asset method.")
+end
+
+
+"""
+    fixing(p::AbstractPath, t::ModelTime, key::String)
+
+Return a fixing from a term structure.
+
+This is used to handle fixings for indices etc.
+"""
+function fixing(p::AbstractPath, t::ModelTime, key::String)
+    error("AbstractPath needs to implement fixing method.")
+end
+
+"""
+    asset_convexity_adjustment(
+        p::AbstractPath,
+        t::ModelTime,
+        T0::ModelTime,
+        T1::ModelTime,
+        T2::ModelTime,
+        key::String
+        )
+
+Return the convexity adjustment for a YoY asset payoff.
+"""
+function asset_convexity_adjustment(
+    p::AbstractPath,
+    t::ModelTime,
+    T0::ModelTime,
+    T1::ModelTime,
+    T2::ModelTime,
+    key::String
+    )
+    error("AbstractPath needs to implement asset_convexity_adjustment method.")
+end
+
+"""
+    forward_index(p::AbstractPath, t::ModelTime, T::ModelTime, key::String)
+
+Expectation E_t^T[S_T] of a tradeable asset.
+"""
+function forward_index(p::AbstractPath, t::ModelTime, T::ModelTime, key::String)
+    error("AbstractPath needs to implement forward_index method.")
+end
+
+"""
+    index_convexity_adjustment(
+        p::AbstractPath,
+        t::ModelTime,
+        T0::ModelTime,
+        T1::ModelTime,
+        T2::ModelTime,
+        key::String
+        )
+
+Return the convexity adjustment for a YoY index payoff.
+"""
+function index_convexity_adjustment(
+    p::AbstractPath,
+    t::ModelTime,
+    T0::ModelTime,
+    T1::ModelTime,
+    T2::ModelTime,
+    key::String
+    )
+    error("AbstractPath needs to implement index_convexity_adjustment method.")
+end
+
+
+"""
+    future_index(p::AbstractPath, t::ModelTime, T::ModelTime, key::String)
+
+Expectation E_t^Q[F(T,T)] of a Future index/price.
+"""
+function future_index(p::AbstractPath, t::ModelTime, T::ModelTime, key::String)
+    error("AbstractPath needs to implement future_index method.")
+end
+
+
+"""
+    swap_rate_variance(
+        p::AbstractPath,
+        t::ModelTime,
+        T::ModelTime,
+        swap_times::AbstractVector,
+        yf_weights::AbstractVector,
+        key::String,
+        )
+
+Calculate the normal model variance of a swap rate via Gaussian
+swap rate approximation.
+"""
+function swap_rate_variance(
+    p::AbstractPath,
+    t::ModelTime,
+    T::ModelTime,
+    swap_times::AbstractVector,
+    yf_weights::AbstractVector,
+    key::String,
+    )
+    error("AbstractPath needs to implement swap_rate_variance method.")
+end
+
+
+"""
+    forward_rate_variance(
+    p::AbstractPath,
+    t::ModelTime,
+    T::ModelTime,
+    T0::ModelTime,
+    T1::ModelTime,
+    key::String,
+    )
+
+Calculate the lognormal model variance of a forward looking or
+backward looking forward rate.
+"""
+function forward_rate_variance(
+    p::AbstractPath,
+    t::ModelTime,
+    T::ModelTime,
+    T0::ModelTime,
+    T1::ModelTime,
+    key::String,
+    )
+    error("AbstractPath needs to implement forward_rate_variance method.")
+end
+
+
+"""
+    asset_variance(
+        p::AbstractPath,
+        t::ModelTime,
+        T::ModelTime,
+        key::String,
+        )
+
+Calculate the lognormal model variance of an asset spot price
+over the time period [t,T].
+"""
+function asset_variance(
+    p::AbstractPath,
+    t::ModelTime,
+    T::ModelTime,
+    key::String,
+    )
+    error("AbstractPath needs to implement asset_variance method.")
+end
+
+
+"""
+    @enum(
+        PathInterpolation,
+        NoPathInterpolation,
+        LinearPathInterpolation,
+    )
+
+PathInterpolation encodes how simulated states can be interpolates.
+"""
+@enum(
+    PathInterpolation,
+    NoPathInterpolation,
+    LinearPathInterpolation,
+)
+
---
+++ src/paths/Path.jl
@@ -0,0 +1,188 @@
+
+"""
+    struct Path <: AbstractPath
+        sim::Simulation
+        ts_dict::Dict{String,<:Termstructure}
+        state_alias_dict::Dict{String,Int}
+        context::Context
+        interpolation::PathInterpolation
+    end
+
+A Path combines a model, simulated model states and term structures. The interface
+to market references is established by a valuation context.
+
+Paths are used by payoffs to calculate simulated zero bonds, asset prices
+and further building blocks of financial instrument payoffs.
+"""
+struct Path <: AbstractPath
+    sim::Simulation
+    ts_dict::Dict{String,<:Termstructure}
+    state_alias_dict::Dict{String,Int}
+    context::Context
+    interpolation::PathInterpolation
+end
+
+"""
+    _check_path_setup(
+        sim::Simulation,
+        ts_dict::Dict{String,Termstructure},
+        cxt::Context,
+        )
+
+Check for consistent path inputs.
+
+We define an extra function in order to avoid chain rule issues with
+automatic differentiation.
+"""
+function _check_path_setup(
+    sim::Simulation,
+    ts_dict::Dict{String,<:Termstructure},
+    cxt::Context,
+    )
+    # all referenced models need to be available
+    @assert isnothing(cxt.numeraire.model_alias) || cxt.numeraire.model_alias in model_alias(sim.model)
+    for entry in values(cxt.rates)
+        @assert isnothing(entry.model_alias) || entry.model_alias in model_alias(sim.model)
+    end
+    for entry in values(cxt.assets)
+        @assert isnothing(entry.asset_model_alias) || entry.asset_model_alias in model_alias(sim.model)
+        @assert isnothing(entry.domestic_model_alias) || entry.domestic_model_alias in model_alias(sim.model)
+        @assert isnothing(entry.foreign_model_alias) || entry.foreign_model_alias in model_alias(sim.model)
+    end
+    # keys and value aliases must be consistent
+    for (key, ts) in ts_dict
+        @assert key == alias(ts)
+    end
+    # all referenced term structures need to be available
+    for alias in unique(values(cxt.numeraire.termstructure_dict))
+        @assert alias in keys(ts_dict)
+        @assert isa(ts_dict[alias], YieldTermstructure)
+    end
+    for entry in values(cxt.rates)
+        for alias in unique(values(entry.termstructure_dict))
+            @assert alias in keys(ts_dict)
+            @assert isa(ts_dict[alias], YieldTermstructure)
+        end
+    end
+    for entry in values(cxt.assets)
+        @assert entry.asset_spot_alias in keys(ts_dict)
+        @assert isa(ts_dict[entry.asset_spot_alias], ParameterTermstructure)
+        for alias in unique(values(entry.domestic_termstructure_dict))
+            @assert alias in keys(ts_dict)
+            @assert isa(ts_dict[alias], YieldTermstructure)
+        end
+        for alias in unique(values(entry.foreign_termstructure_dict))
+            @assert alias in keys(ts_dict)
+            @assert isa(ts_dict[alias], YieldTermstructure)
+        end
+    end
+    for entry in values(cxt.forward_indices)
+        @assert entry.forward_index_alias in keys(ts_dict)
+        @assert isa(ts_dict[entry.forward_index_alias], ParameterTermstructure)
+    end
+    for entry in values(cxt.future_indices)
+        @assert entry.future_index_alias in keys(ts_dict)
+        @assert isa(ts_dict[entry.future_index_alias], ParameterTermstructure)
+    end
+    for entry in values(cxt.fixings)
+        @assert entry.termstructure_alias in keys(ts_dict)
+        @assert isa(ts_dict[entry.termstructure_alias], ParameterTermstructure)
+    end
+end
+
+"""
+    path(
+        sim::Simulation,
+        ts_dict::Dict{String,<:Termstructure},
+        cxt::Context,
+        ip::PathInterpolation = NoPathInterpolation
+        )
+
+Create a Path object.
+"""
+function path(
+    sim::Simulation,
+    ts_dict::Dict{String,<:Termstructure},
+    cxt::Context,
+    ip::PathInterpolation = NoPathInterpolation
+    )
+    _check_path_setup(sim, ts_dict, cxt)
+    state_alias_dict = alias_dictionary(state_alias(sim.model))
+    return Path(sim, ts_dict, state_alias_dict, cxt, ip)
+end
+
+"""
+    _check_unique_ts(ts_alias::AbstractVector)
+
+Check for consistent term structure inputs to path.
+
+We define an extra function in order to avoid chain rule issues with
+automatic differentiation.
+"""
+function _check_unique_ts(ts_alias::AbstractVector)
+    @assert length(ts_alias) == length(unique(ts_alias))
+end
+
+"""
+    path(
+        sim::Simulation,
+        ts::Vector{Termstructure},
+        cxt::Context,
+        ip::PathInterpolation = NoPathInterpolation
+        )
+
+Create a Path object from a list of term structures.
+"""
+function path(
+    sim::Simulation,
+    ts::Vector{<:Termstructure},
+    cxt::Context,
+    ip::PathInterpolation = NoPathInterpolation
+    )
+    #
+    ts_alias = [ alias(ts_) for ts_ in ts ]
+    _check_unique_ts(ts_alias)
+    #
+    ts_dict = Dict{String,Termstructure}(((alias(ts_), ts_) for ts_ in ts))
+    return path(sim, ts_dict, cxt, ip)
+end
+
+"""
+    length(p::Path)
+
+Derive the number of realisations from the linked simulation.
+"""
+function length(p::Path)
+    return size(p.sim.X)[2]
+end
+
+"""
+    state_variable(sim::Simulation, t::ModelTime, ip::PathInterpolation)
+
+Derive a state variable for a given observation time.
+"""
+function state_variable(sim::Simulation, t::ModelTime, ip::PathInterpolation)
+    eps = 0.5 / 365  # we want to give a bit tolerance in avoiding interpolation
+    idx = searchsortedfirst(sim.times, t)
+    if idx <= length(sim.times) && t > sim.times[idx] - eps
+        return @view sim.X[:,:,idx]
+    end
+    if idx > 1 && t < sim.times[idx-1] + eps
+        return @view sim.X[:,:,idx-1]
+    end
+    # if we end up here we need some interpolation/extrapolation
+    @assert ip != NoPathInterpolation
+    # we assume constant extrapolation
+    if idx == 1
+        return @view sim.X[:,:,idx]
+    end
+    if idx > length(sim.times)
+        return @view sim.X[:,:,idx-1]
+    end
+    # now we have 1 < idx <= length(sim.times)
+    if ip == LinearPathInterpolation
+        rho = (t - sim.times[idx-1]) / (sim.times[idx] - sim.times[idx-1])
+        return rho * @view(sim.X[:,:,idx]) + (1.0-rho) * @view(sim.X[:,:,idx-1])
+    end
+    error("Unknown PathInterpolation.")
+end
