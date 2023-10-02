e5c644d3f1ec57c177816c5f6afc554af20f6b2c
Sebastian Schlenkrich <sebastian.schlenkrich@frame-consult.de>
2023-10-02 14:12:04 +0200

    Allow use of test_arg for runtest testing

--- test/runtests.jl
+++ test/runtests.jl
@@ -9,7 +9,16 @@ using Logging
 
 @testset verbose=true "DiffFusion.jl" begin
 
-    include("unittests/unittests.jl")
+    if @isdefined(ARGS) && length(ARGS) > 0
+        @info "Run tests " * ARGS[1] * " from test_args."
+        @testset verbose=true "Runtests" begin
+            include(ARGS[1])
+        end
+    else
+        @testset verbose=true "Unit tests" begin
+            include("unittests/unittests.jl")
+        end
+    end
 
 end
 
