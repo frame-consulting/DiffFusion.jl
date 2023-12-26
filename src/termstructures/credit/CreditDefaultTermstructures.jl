a72d87383d8f744b5cd9bf4345d251dccb4d6f62
Sebastian Schlenkrich <sebastian.schlenkrich@frame-consult.de>
2023-12-26 09:54:09 +0100

    Add FlatSpreadCurve and LogSurvivalCurve

---
+++ src/termstructures/credit/CreditDefaultTermstructures.jl
@@ -0,0 +1,115 @@
+
+"""
+    struct FlatSpreadCurve <: CreditDefaultTermstructure
+        alias::String
+        spread::ModelValue
+    end
+
+A flat credit spread curve.
+"""
+struct FlatSpreadCurve <: CreditDefaultTermstructure
+    alias::String
+    spread::ModelValue
+end
+
+"""
+    flat_spread_curve(alias::String, spread::ModelValue)
+
+Create a FlatSpreadCurve.
+"""
+function flat_spread_curve(alias::String, spread::ModelValue)
+    return FlatSpreadCurve(alias, spread)
+end
+
+
+"""
+    flat_spread_curve(spread::ModelValue)
+
+Create a FlatSpreadCurve without alias.
+"""
+function flat_spread_curve(spread::ModelValue)
+    return FlatSpreadCurve("", spread)
+end
+
+
+"""
+survival(ts::FlatSpreadCurve, t::ModelTime)
+
+Calculate survival probability.
+"""
+survival(ts::FlatSpreadCurve, t::ModelTime) = exp(-ts.spread * t)
+
+
+
+"""
+    struct LogSurvivalCurve <: CreditDefaultTermstructure
+        alias::String
+        times::AbstractVector
+        values::AbstractVector
+    end
+
+Log-interpolated survival probabilities.
+"""
+struct LogSurvivalCurve <: CreditDefaultTermstructure
+    alias::String
+    times::AbstractVector
+    values::AbstractVector
+    interpolation
+end
+
+
+"""
+    survival_curve(
+        alias::String,
+        times::AbstractVector,
+        survival_probs::AbstractVector,
+        interp_method = (x,y) -> linear_interpolation(x, y, extrapolation_bc = Line()),
+        )
+
+Create a LogSurvivalCurve.
+"""
+function survival_curve(
+    alias::String,
+    times::AbstractVector,
+    survival_probs::AbstractVector,
+    interp_method = (x,y) -> linear_interpolation(x, y, extrapolation_bc = Line()),
+    )
+    @assert length(times) > 0
+    @assert length(times) == length(survival_probs)
+    if times[begin] != 0.0
+        @warn "First time should typically be 0.0." times[begin]
+    end
+    if survival_probs[begin] != 1.0
+        @warn "First survival probability should typically be 1.0." survival_probs[begin]
+    end
+    values = log.(survival_probs)
+    return LogSurvivalCurve(alias, times, values, interp_method(times, values))
+end
+
+
+"""
+    survival_curve(
+        alias::String,
+        times::AbstractVector,
+        survival_probs::AbstractVector,
+        method_alias::String,
+        )
+
+Create a LogSurvivalCurve for pre-defined interpolation methods.
+"""
+function survival_curve(
+    alias::String,
+    times::AbstractVector,
+    survival_probs::AbstractVector,
+    method_alias::String,
+    )
+    return survival_curve(alias, times, survival_probs, interpolation_methods[uppercase(method_alias)])
+end
+
+
+"""
+    survival(ts::LogSurvivalCurve, t::ModelTime)
+
+Calculate survival probability.
+"""
+survival(ts::LogSurvivalCurve, t::ModelTime) = exp(ts.interpolation(t))
--- test/unittests/termstructures/credit/credit.jl
+++ test/unittests/termstructures/credit/credit.jl
@@ -3,6 +3,31 @@ using Test
 
 @testset "Credit default curves." begin
 
-    # Add tests here.
+    @testset "Test FlatSpreadCurve." begin
+        cv = DiffFusion.flat_spread_curve("Std", 0.05)
+        @test DiffFusion.alias(cv) == "Std"
+        @test cv(1.0) == exp(-0.05)
+        #
+        cv = DiffFusion.flat_spread_curve(0.05)
+        @test DiffFusion.alias(cv) == ""
+        @test cv(1.0) == exp(-0.05)
+    end
+
+
+    @testset "Test LogSurvivalCurve." begin
+        times = [0.0, 2.0, 5.0]
+        survival_probs = [1.0, 0.5, 0.25]
+        cv = DiffFusion.survival_curve("Std", times, survival_probs)
+        cv = DiffFusion.survival_curve("Std", times, survival_probs, "CUBIC")
+        #
+        cv = DiffFusion.survival_curve("Std", times, survival_probs)
+        #
+        @test cv(0.0) == 1.0
+        @test cv(2.0) == 0.5
+        @test cv(5.0) == 0.25
+        #
+        @test cv(1.0) == exp(-0.34657359027997264)
+        @test isapprox(cv(3.0), 0.5 * exp(-0.23104906018664842), atol=1.0e-14)
+    end
 
 end
