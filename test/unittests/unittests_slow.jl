19a488fad19283679d8f1472ae1081aca552240a
Sebastian Schlenkrich <sebastian.schlenkrich@frame-consult.de>
2025-11-21 22:41:05 +0100

    Separate Zygote in unittests

--- test/unittests/unittests_slow.jl
+++ test/unittests/unittests_slow.jl
@@ -6,6 +6,10 @@ using Test
 @testset verbose=true "unittests_slow.jl" begin
 
     include("analytics/valuations.jl")
+    if VERSION < v"1.12"
+        # Zygote yields segfault with v1.12
+        include("analytics/valuations_zygote.jl")
+    end
 
 end
 
